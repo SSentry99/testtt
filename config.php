1f4d2baec3ce2fe06ad17addf6673da58092609a
SSentry99 <108374031+SSentry99@users.noreply.github.com>
2024-08-25 19:29:35 -0400

    Add files via upload

---
+++ config.php
@@ -0,0 +1,38 @@
+<?php
+
+	//Site url - add / at the end, eg: http://metin2cms.cf/mt2/ IMPORTANT!!!
+
+	$site_url = "http://127.0.0.1/";
+
+	
+
+	//Game database
+
+	$host = "217.113.49.32";
+
+	$user = "root";
+
+	$password = "d877d79f473cb";
+
+	
+
+	//Mail settings
+
+	$SMTPAuth = true;
+
+	$SMTPSecure = "ssl";
+
+	$EmailHost = "mail.fa2.ro";
+
+	$emailPort = 465;
+
+	
+
+	$email_username = "scrisoare@fa2.ro";//gmail account
+
+	$email_password = "F%b3@Basdfvbomf+";//gmail password
+	
+
+	//Register
+
+	$safebox_size = 1;
